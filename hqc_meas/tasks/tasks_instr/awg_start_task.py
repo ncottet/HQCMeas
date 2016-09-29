f998d43f543439841f27c47627cb64bdd72bb32d
jonsm <sanmijon@mac.com>
2016-09-29 10:58:59 -0400

    Add stop function in AWG start

--- hqc_meas/tasks/tasks_instr/awg_start_task.py
+++ hqc_meas/tasks/tasks_instr/awg_start_task.py
@@ -7,31 +7,48 @@
 """
 """
 from hqc_meas.tasks.api import (InstrumentTask)
-
+from inspect import cleandoc
+from atom.api import (Str)
 
 class AWGStartTask(InstrumentTask):
-    """Start the AWG pulse sequence loop after setting the trigger as required. 
-    Need to be called after Phase Alazar task. 
+    """Start or stop the AWG pulse sequence loop after setting the trigger as required. 
+    Need to be called after Phase Alazar task (for turning on) or after 
+    Phase Alazar Task is over (for turning off).
     """
 
     driver_list = ['AWG5014B']
+    
+    on_off = Str().tag(pref=True)
 
     def check(self, *args, **kwargs):
         """
+        Check that the user entered 0 or 1 and nothing else
         """
-        return True, {}
+        test, traceback = super(AWGStartTask, self).check(*args, **kwargs)
+                                                             
+        if self.on_off != '0' and self.on_off != '1':
+            test = False
+            traceback[self.task_path + '/' + self.task_name] = \
+                                    cleandoc('''Enter 0 for OFF, 1 for ON''')
+        
+        return test, traceback
 
     def perform(self):
         """
-        Simply turning on the AWG
+        Simply turning on or off the AWG
         """
         if not self.driver:
             self.start_driver()
         running_state = self.driver.running
-        if running_state != '0 : Instrument has stopped':
-            print 'WARNING: AWG already running'
-            
-        self.driver.running = 'RUN'
+        if self.on_off == '0':
+            if running_state != '2 : Intrument is running':
+                print 'WARNING: AWG already stopped or waiting for a trigger'
+            self.driver.running = 'STOP'
+        
+        if self.on_off == '1':
+            if running_state != '0 : Instrument has stopped':
+                print 'WARNING: AWG already running'           
+            self.driver.running = 'RUN'
         
        
 KNOWN_PY_TASKS = [AWGStartTask]
